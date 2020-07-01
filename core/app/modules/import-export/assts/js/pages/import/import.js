b31693d9e43480ae435f3ee7a84392db01716c4a
Rotem Ben-Itzhak <rotem@elementor.com>
2020-07-01 11:00:10 +0300

    Adding drag and drop component t import.

--- core/app/modules/import-export/assts/js/pages/import/import.js
+++ core/app/modules/import-export/assts/js/pages/import/import.js
@@ -1,4 +1,5 @@
 import Layout from '../../templates/layout';
+import DragDrop from './drag-drop/drag-drop';
 import Message from '../../ui/message/message';
 import Heading from '../../ui/heading/heading';
 import Text from '../../ui/text/text';
@@ -8,21 +9,27 @@ import Button from 'elementor-app/ui/molecules/button';
 import './import.scss';
 
 export default function Import() {
+	const onDragOver = ( event ) => {
+		console.log( 'event', event );
+	};
+
 	return (
 		<Layout type="import">
 			<section className="e-app-import">
-				<Message className="e-app-import__select-file">
-					<Heading size="lg">
-						{ __( 'Import a Kit to Your Site', 'elementor' ) }
-					</Heading>
-					<Text size="md">
-						{ __( 'Drag & Drop your zip template file', 'elementor' ) }
-					</Text>
-					<Text size="sm">
-						{ __( 'Or', 'elementor' ) }
-					</Text>
-					<Button variant="primary" text={ __( 'Select File', 'elementor' ) } url="/#" />
-				</Message>
+				<DragDrop>
+					<Message className="e-app-import__select-file">
+						<Heading size="lg">
+							{ __( 'Import a Kit to Your Site', 'elementor' ) }
+						</Heading>
+						<Text size="md">
+							{ __( 'Drag & Drop your zip template file', 'elementor' ) }
+						</Text>
+						<Text size="sm">
+							{ __( 'Or', 'elementor' ) }
+						</Text>
+						<Button variant="primary" text={ __( 'Select File', 'elementor' ) } url="/#" />
+					</Message>
+				</DragDrop>
 
 				<Box type="notice">
 					{ __( 'Important: It is strongly recommended that you backup your database before Importing a Kit.', 'elementor' ) }
