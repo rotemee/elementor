825d6fe21c73881de9604705678f4a0c0b64e4d6
Rotem Ben-Itzhak <rotem@elementor.com>
2020-12-08 16:53:07 +0200

    Basic Infrastructure - With Material UI Button.

--- core/app/modules/site-editor/assets/js/package.js
+++ core/app/modules/site-editor/assets/js/package.js
@@ -1,7 +1,7 @@
 // Alphabetical order.
 import AllPartsButton from './organisms/all-parts-button';
 import Layout from './templates/layout';
-import Module from './module';
+//import Module from './module';
 import NotFound from './pages/not-found';
 import SiteParts from './organisms/site-parts';
 import SitePart from './molecules/site-part';
@@ -10,7 +10,7 @@ import { Context as TemplateTypesContext } from './context/template-types';
 export default {
 	AllPartsButton,
 	Layout,
-	Module,
+	//Module,
 	NotFound,
 	SitePart,
 	SiteParts,
