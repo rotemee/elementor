2aafc0c714785e1edc138644828643f9467a5623
Rotem Ben-Itzhak <rotem@elementor.com>
2020-12-08 19:02:55 +0200

    Overriding component style to create additional variation.

--- core/app/assets/js/ui/molecules/button.js
+++ core/app/assets/js/ui/molecules/button.js
@@ -2,9 +2,25 @@ import { Link, LocationProvider } from '@reach/router';
 import router from '@elementor/router';
 import Icon from 'elementor-app/ui/atoms/icon';
 
+import { withStyles } from '@material-ui/core/styles';
 import { default as MuButton } from '@material-ui/core/Button';
 
 import './buttons.scss';
+
+const StyledButton = withStyles( {
+	root: {
+		background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+		borderRadius: 3,
+		border: 0,
+		color: 'white',
+		height: 48,
+		padding: '0 30px',
+		boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+	},
+	label: {
+		textTransform: 'capitalize',
+	},
+} )( MuButton );
 
 export default class Button extends React.Component {
 	static propTypes = {
